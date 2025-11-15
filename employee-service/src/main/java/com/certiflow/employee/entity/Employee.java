1e899bc9d11039740430e203affe7473a686f2e3
razvan700 <110129667+razvan700@users.noreply.github.com>
2025-11-16 01:48:17 +0200

    Created employee entity

---
+++ employee-service/src/main/java/com/certiflow/employee/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.certiflow.employee.entity;
+
+import com.certiflow.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee extends BaseEntity {
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String department;
+
+    private boolean active = true;
+}
